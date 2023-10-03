072d6a2804a8b8f0e19d466a742898f3b1121c81
Celiian <90334525+Celiian@users.noreply.github.com>
2023-10-03 19:51:51 +0200

    feat/ added answer and progress bar

--- BlindTestIos/GameViewController.swift
+++ BlindTestIos/GameViewController.swift
@@ -10,69 +10,106 @@ import AVFoundation
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     var artist: [String: Any]?
-    var previews_url : [String] = []
+    var previews_url: [[String: String]] = []
     var audioPlayer: AVAudioPlayer?
-        
-        @IBOutlet weak var playButton: UIButton!
-
-        
-        var player: AVPlayer?
-        var timer: Timer?
+    @IBOutlet weak var music_name: UILabel!
     
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            // Now, you can access the 'artist' dictionary here.
-            getSongs()
-        }
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    
+    var player: AVPlayer?
+    var timer: Timer?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.progress.isHidden = true
+        self.music_name.isHidden = true
+        getSongs()
+    }
     
     
     
     func getSongs () {
         let artistId = artist!["id"] as? String
-    
+        spinner.startAnimating()
+        
         getSpotify(type: "artists", parameter: artistId!, parameterType: "/top-tracks?market=Fr") { result in
             if let result = result {
                 if let data = result.data(using: .utf8) {
                     do {
                         if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                                                  let tracks = json["tracks"] as? [[String: Any]] {
-                                                   
-                                                   let previewURLs = tracks.compactMap { track in
-                                                       return track["preview_url"] as? String
-                                                   }
-
-                                                   for url in previewURLs {
-                                                       self.previews_url.append(url)
-                                                   }
-                                               }
-                                           } catch {
-                                               print(error.localizedDescription)
-                                           }
+                           let tracks = json["tracks"] as? [[String: Any]] {
+                            
+                            let previewDictionary = tracks.compactMap { track in
+                                return ["url" : track["preview_url"] as? String ?? "", "name" : track["name"] as? String ?? ""]
+                            }
+                            
+                            self.previews_url = previewDictionary
+                        }
+                        
+                    } catch {
+                        print(error.localizedDescription)
+                    }
                 }
             } else {
                 print("Error or nil result")
             }
         }
         
+        
+        let audioDuration = 10.0
+        let timerDuration = 15.0
         var index = 0
-        timer = Timer.scheduledTimer(withTimeInterval: 15, repeats: true) { [weak self] _ in
-            do {
-                self?.player?.pause()
-                if(index < (self?.previews_url.count)!){
-                    if let urlString = self?.previews_url[index], let audioURL = URL(string: urlString) {
-                        self?.player = AVPlayer(url: audioURL)
-                        self?.player?.play()
+        var progressTimer: Timer? // Declare progressTimer here
+
+        timer = Timer.scheduledTimer(withTimeInterval: timerDuration, repeats: true) { [weak self] _ in
+            self?.player?.pause()
+            if index < (self?.previews_url.count)! {
+             
+                if let urlString = self?.previews_url[index]["url"], let audioURL = URL(string: urlString),
+                   let songName = self?.previews_url[index]["name"]{
+                    self?.player = AVPlayer(url: audioURL)
+                    self?.spinner.stopAnimating()
+                    self?.spinner.isHidden = true
+                    self?.progress.isHidden = false
+                    
+                    self?.music_name.text = ""
+                    self?.music_name.isHidden = true
+                    self?.player?.play()
+                    
+                    // Pause audio after the desired audio duration
+                    DispatchQueue.main.asyncAfter(deadline: .now() + audioDuration) {
+                        self?.player?.pause()
                     }
-                    index += 1
+                    
+                    // Update progress bar every second
+                    var seconds = 0.0
+                    self?.progress.setProgress(0, animated: true)
+                    progressTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+                        guard let self = self else { return }
+                        let progressValue = Float(seconds) / Float(audioDuration)
+                        self.progress.setProgress(progressValue, animated: true)
+                        seconds += 0.1
+                        
+                         
+                        if Int(seconds) > Int(audioDuration) {
+                            self.music_name.isHidden = false
+                            self.music_name.text = songName
+                            progressTimer?.invalidate()
+                           
+                        }
+                    }
                 }
+                index += 1
             }
-            catch {
-                print("Error initializing AVPlayer: \(error.localizedDescription)")
-            }
+            
         }
+
     }
     
     
