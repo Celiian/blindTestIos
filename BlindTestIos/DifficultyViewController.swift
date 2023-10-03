35fc5ff54987805650566e69405d101a399ba25b
Iagomorph <philemon.wild@gmail.com>
2023-10-03 10:40:13 +0200

    feat(base): added base UiControllerViews & base storyboard

---
+++ BlindTestIos/DifficultyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DifficultyViewController.swift
+//  BlindTestIos
+//
+//  Created by Philémon Wild on 03/10/2023.
+//
+
+import UIKit
+
+class DifficultyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
